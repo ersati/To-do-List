d6fc5685628c7f42255b3386133107c9ef6f9ba8
Piotr Pacewicz <piotrp92@gmail.com>
2021-02-03 18:07:58 +0000

    Removed node_module folder

--- app.js
+++ app.js
@@ -4,29 +4,49 @@ const data = require('./date')
 const app = express();
 
 // const jsonParser = bodyParser.json()
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.set('view engine', 'ejs');
 
 const dataArr = ['zenek', 'Juziek', 'Zbyszek']
-
-app.get('/', function(req,res){
+const workArr = [];
 app.use(express.static("public"))
-const day = data.getDate()
-    res.render('list', {kindDay: day, listOftask: dataArr})
+app.get('/', function (req, res) {
+
+    const day = data.getDate()
+    res.render('list', {
+        kindDay: day,
+        listOftask: dataArr
+    })
 })
 
-app.post("/", function(req,res){
-const newTask = req.body.newItem;
-if(newTask === '' || typeof newTask === undefined || typeof newTask === null){
-     console.log('empty string');
-     res.redirect('/')
-} else {
-    dataArr.push(newTask)
-    console.log(newTask)
-    res.redirect('/')
-}
+app.post("/", function (req, res) {
+    const newTask = req.body.newItem;
+    const btn = req.body.button
+    console.log(req.body)
+    if (newTask === '' || typeof newTask === undefined || typeof newTask === null) {
+        console.log('empty string');
+        res.redirect('/')
+    } else {
 
+        if(btn === 'Work'){
+            workArr.push(newTask)
+            res.redirect('/work')
+        } else{
+            dataArr.push(newTask)
+            console.log(newTask)
+            res.redirect('/')
+        }
+        
+    }
+})
 
+app.get('/work', function(req, res){
+    res.render('list', {
+        kindDay: 'Work',
+        listOftask: workArr
+    })
 })
 
 
@@ -35,8 +55,6 @@ if(newTask === '' || typeof newTask === undefined || typeof newTask === null){
 
 
 
-
-
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('pete servers works')
 })
